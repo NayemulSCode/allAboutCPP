dd92491aca8aeffb8204d808c82b6c5a3d2e3e4d
Nayemul <nayemulhasan2016@gmail.com>
2021-07-25 11:42:24 +0600

    1020

---
+++ uri problem solving/at-1_age_in_days_1020.cpp
@@ -0,0 +1,11 @@
+//with compiler error
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N;
+    cin>>N;
+    cout<<N/365<<" ano(s)"<<endl<<N%365/30<<" mes(es)"<<endl<<N%365%30<<" dia(s)"<<endl;
+    return 0;
+}
+
