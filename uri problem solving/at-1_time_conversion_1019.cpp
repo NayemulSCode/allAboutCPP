6a143c67c68f895c9427838e2851424342b9fb05
Nayemul <nayemulhasan2016@gmail.com>
2021-07-25 11:14:42 +0600

    1019

--- uri problem solving/at-1_time_conversion_1019.cpp
+++ uri problem solving/at-1_time_conversion_1019.cpp
@@ -1,1 +1,12 @@
-
+///int hours = (int) (time / 3600);
+///int minutes = ((int) (time / 60)) % 60;
+///int seconds = time % 60;
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N;
+    cin>>N;
+    cout<<N/3600<<":"<<N/60%60<<":"<<N%60<<endl;
+    return 0;
+}
